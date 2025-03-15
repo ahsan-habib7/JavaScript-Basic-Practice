09ed511ba36eb8e8275647e1611f0d55cf5e6923
S. M Ahsan Habib <136216079+ahsan-habib7@users.noreply.github.com>
2025-03-15 09:52:56 +0600

    Printing Processes(JS)

---
+++ print.js
@@ -0,0 +1,37 @@
+// Print a string
+console.log("Hello, World!");
+
+// Print a number
+console.log(123);
+
+// Print a boolean
+console.log(true);
+
+// Print an array
+console.log([1, 2, 3, 4, 5]);//or
+console.log(["John", "Jane", "Jack"]);
+const arr = [6 , 16, 26, 36, 46];
+console.log(arr);
+
+// Print an object
+console.log({ name: "John", age: 30 });
+
+// Print multiple values
+console.log("Name:", "John", "Age:", 30);
+
+// Print an error message
+console.error("This is an error message");
+
+// Print a warning message
+console.warn("This is a warning message");
+
+// Print an info message
+console.info("This is an info message");
+
+// Print a table
+console.table([{ name: "John", age: 30 }, { name: "Jane", age: 25 }]);
+
+// Print formatted string using template literals
+let Name = "John";
+let age = 300;
+console.log(`Name: ${Name}, Age: ${age}`);
